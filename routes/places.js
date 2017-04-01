0dc6f6cbb1d843643b590642e747d2cc85d82f0b
Amir Fayek <amir@amirfayek.com>
2017-04-01 15:12:51 -0700

    add place search form

--- routes/places.js
+++ routes/places.js
@@ -29,7 +29,13 @@ router.get('/', function(req, res, next) {
   //           console.log(JSON.stringify(response, null, 2));
   //         }
   //    });
-  res.render('places', { city: 'Stanford' });
+  res.render('places-form', {});
+});
+
+router.post('/', function(req, res, next) {  
+    var redirectUrl = '/places/' + req.body.inputName
+    console.log(redirectUrl);
+    res.redirect(redirectUrl);
 });
 
 router.get('/:name', function(req, res, next) {  
@@ -40,48 +46,15 @@ router.get('/:name', function(req, res, next) {
     headers: {
         'Ocp-Apim-Subscription-Key': process.env.BING_KEY
     }
-  }
+}
 
-  function searchBing(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      _.extend(place, {news: info})
-      console.log(info);
-      place.news = info;
-      // res.render('places', { city: req.params.name, news: info });
-    }
-  }
-
-  //   var enriched_text = {};
-  //   enriched_text.entities = {};
-
-  // // watson config
-  // var discovery = new DiscoveryV1({
-  //   username: '495b42a9-c5ef-4d96-a95f-b67df49c6284',
-  //   password: 'rJpkRmmyDXGG',
-  //   version_date: DiscoveryV1.VERSION_DATE_2016_12_15
-  // });
-
-  // discovery.query({
-  //     environment_id: 'db230d72-27d8-429a-b78f-059fbca84041',
-  //     collection_id: '21a7e4a0-d9b2-4f0d-91ba-0ff26815e6b6'
-  //   }, function(err, response) {
-  //         if (err) {
-  //           console.error(err);
-  //         } else {
-  //           console.log(JSON.stringify(response, null, 2));
-  //         }
-  //    });
-
-  rp(bingOptions).then(function(data) {
+rp(bingOptions).then(function(data) {
     var info = JSON.parse(data);
     _.extend(place, {news: info});
-  }).then(function() {
+}).then(function() {
     console.log(place);
     res.render('places', place);
-  })
-  // request(bingOptions, searchBing);
-
+})
 });
 
 module.exports = router;
