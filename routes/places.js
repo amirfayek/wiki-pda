a94f0074fe8258f337768cf1de70c67d1a999e1b
Amir Fayek <amir@amirfayek.com>
2017-04-01 13:54:10 -0700

    add bing news api and dotenv for token mgmt

--- routes/places.js
+++ routes/places.js
@@ -1,9 +1,87 @@
 var express = require('express');
 var router = express.Router();
 
+var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+var request = require('request');
+var rp = require('request-promise');
+var _ = require('underscore');
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('places', { city: 'San Francisco' });
+router.get('/', function(req, res, next) {  
+  //   var enriched_text = {};
+  //   enriched_text.entities = {};
+
+  // // watson config
+  // var discovery = new DiscoveryV1({
+  //   username: '495b42a9-c5ef-4d96-a95f-b67df49c6284',
+  //   password: 'rJpkRmmyDXGG',
+  //   version_date: DiscoveryV1.VERSION_DATE_2016_12_15
+  // });
+
+  // discovery.query({
+  //     environment_id: 'db230d72-27d8-429a-b78f-059fbca84041',
+  //     collection_id: '21a7e4a0-d9b2-4f0d-91ba-0ff26815e6b6'
+  //   }, function(err, response) {
+  //         if (err) {
+  //           console.error(err);
+  //         } else {
+  //           console.log(JSON.stringify(response, null, 2));
+  //         }
+  //    });
+  res.render('places', { city: 'Stanford' });
+});
+
+router.get('/:name', function(req, res, next) {  
+  var place = { name: req.params.name };
+
+  var bingOptions = {
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=' + req.params.name + '&count=10&offset=0&mkt=en-us&safeSearch=Moderate',
+    headers: {
+        'Ocp-Apim-Subscription-Key': process.env.BING_KEY
+    }
+  }
+
+  function searchBing(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      _.extend(place, {news: info})
+      console.log(info);
+      place.news = info;
+      // res.render('places', { city: req.params.name, news: info });
+    }
+  }
+
+  //   var enriched_text = {};
+  //   enriched_text.entities = {};
+
+  // // watson config
+  // var discovery = new DiscoveryV1({
+  //   username: '495b42a9-c5ef-4d96-a95f-b67df49c6284',
+  //   password: 'rJpkRmmyDXGG',
+  //   version_date: DiscoveryV1.VERSION_DATE_2016_12_15
+  // });
+
+  // discovery.query({
+  //     environment_id: 'db230d72-27d8-429a-b78f-059fbca84041',
+  //     collection_id: '21a7e4a0-d9b2-4f0d-91ba-0ff26815e6b6'
+  //   }, function(err, response) {
+  //         if (err) {
+  //           console.error(err);
+  //         } else {
+  //           console.log(JSON.stringify(response, null, 2));
+  //         }
+  //    });
+
+  rp(bingOptions).then(function(data) {
+    var info = JSON.parse(data);
+    _.extend(place, {news: info});
+  }).then(function() {
+    console.log(place);
+    res.render('places', place);
+  })
+  // request(bingOptions, searchBing);
+
 });
 
 module.exports = router;
